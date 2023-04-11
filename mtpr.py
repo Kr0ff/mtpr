0c37dfea769f1444e9f21f9dfab5b48ba769cf4d
Kr0ff <35173514+Kr0ff@users.noreply.github.com>
2023-04-11 10:10:16 +0100

    Minor changes

--- mtpr.py
+++ mtpr.py
@@ -4,7 +4,11 @@ import os
 import sys
 import json
 import pathlib
-import git # GitPython
+
+try:
+    import git # GitPython
+except ImportError as e:
+    raise Exception(f"[-] Missing required python library - \n {e}")
 
 # __version__ = 0.2
 
@@ -14,7 +18,7 @@ def isfile(json_file):
     file = False
 
     if os.path.isfile(json_file):
-        file = True    
+        file = True
 
     return file
 
@@ -24,17 +28,16 @@ def ispackage_git(_json):
     _isgit = False
 
     '''
-
     check_isfile = isfile(json_file)
     if check_isfile == False:
-        print("Specified argument is not a file")
+        print("[-] Specified argument is not a file")
         sys.exit(-1)
 
     json_file = pathlib.Path(json_file).resolve()
 
     with open(json_file, "r") as _package:
         _json = json.load(_package)
-        
+
         if _json["git"] == True:
             _isgit = True
     '''
@@ -56,13 +59,13 @@ def parseJson(json_file):
     # Do file check
     check_isfile = isfile(json_file)
     if check_isfile == False:
-        print("Specified argument is not a file")
+        print("[-] Specified argument is not a file")
         sys.exit(-1)
 
     # Resolve the file location
     json_file = pathlib.Path(json_file).resolve()
 
-    # open the package file 
+    # open the package file
     with open(json_file, "r") as _package:
         _json = json.load(_package)
 
@@ -70,20 +73,22 @@ def parseJson(json_file):
 
 # Performs the cloning of repos
 def gitcloner(json_file):
-    
+
+    print(f"[+] Starting repository cloning - ({json_file})")
+
     _json = parseJson(json_file)
     _package_ = ""
-    
+
     _not_git_package = []
 
     # Loop through all packages in the package.json file
     for repo in _json:
         _package_ = _json[repo]
-    
+
         # Check if package is clonable (git valid)
         _git_check = ispackage_git(_package_)
         if _git_check == True:
-            
+
             # Set the vars for each attribute of a package
             _git_repo_name = _package_["name"]
             _git_url = _package_["url"]
@@ -97,23 +102,23 @@ def gitcloner(json_file):
             except Exception as e:
                 print("- Cloning failed")
                 print(e)
-        
+
         else:
-            # Append package to the exclusion list 
+            # Append package to the exclusion list
             # if package is not git clonable
             _not_git_package.append(_package_['name'])
 
     # Check if any non-git packages were found
     if len(_not_git_package) == 0:
-        print("No non-git package(s) found")
+        print("[-] No non-git package(s) found")
     else:
-        print("The following package(s) were excluded:")
+        print("[!] The following package(s) were excluded:")
         for p in _not_git_package:
             print(f"\t- [ {p} ]")
 
 if __name__ == "__main__":
-    
-    _helpmsg = "Usage: mtpr.py </path/to/package.json>"
+
+    _helpmsg = "[*] Usage: mtpr.py </path/to/package.json>"
 
     if len(sys.argv) < 2:
         print(_helpmsg)
